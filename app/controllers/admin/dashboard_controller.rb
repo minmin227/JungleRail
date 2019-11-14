3035d4730b92265831920666b3669fdb20708237
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-11-14 14:01:42 +0000

    add feature dasboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,3 +1,8 @@
 class Admin::DashboardController < ApplicationController
-   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
+   def show
+      @total_products = Product.count
+      @total_categories = Category.count
+   end
+   
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def index
     @products = Product.order(id: :desc).all
   end
